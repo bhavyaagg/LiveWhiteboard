30438fec0981c10ff1a6d34076058e5dea2a7bf3
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-01-13 17:36:51 +0530

    create constants for database
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

---
+++ app/src/main/java/com/example/apoorvaagupta/livewhiteboard/Constants.java
@@ -0,0 +1,22 @@
+package com.example.apoorvaagupta.livewhiteboard;
+
+/**
+ * Created by apoorvaagupta on 13/01/18.
+ */
+
+public interface Constants {
+
+    String COMMA = " , ";
+    String LBR = " ( ";
+    String RBR = " ) ";
+    String SEMI = " ; ";
+
+    String CMD_CREATE_TABLE_INE = " CREATE TABLE IF NOT EXISTS ";
+
+    String TYPE_INT = " INTEGER ";
+    String TYPE_TEXT = " TEXT ";
+    String TYPE_BOOLEAN = " BOOLEAN ";
+    String TYPE_BLOB = " BLOB ";
+    String TYPE_PK_AI = " PRIMARY KEY AUTOINCREMENT ";
+
+}
