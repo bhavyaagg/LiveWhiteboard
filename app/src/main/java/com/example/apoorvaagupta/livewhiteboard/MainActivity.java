da79b37cde8d8bee69cc61584f82b84bb7a3384b
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-13 22:14:51 +0530

    Update MainActivity and use sockets
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- app/src/main/java/com/example/apoorvaagupta/livewhiteboard/MainActivity.java
+++ app/src/main/java/com/example/apoorvaagupta/livewhiteboard/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.apoorvaagupta.livewhiteboard;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MAIN";
+
     TextView startNewSession, joinExistingSession, createNewDrawing, openExistingDrawing;
 
     Socket socket;
@@ -20,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         SocketHandler app = (SocketHandler) getApplication();
         socket = app.getSocket();
-
+        socket.connect();
 
         startNewSession = findViewById(R.id.tvStartNewSession);
         joinExistingSession = findViewById(R.id.tvJoinExistingSession);
@@ -32,6 +40,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //send request to the server for creating a new room
                 //the id returned by the server will be put in the bundle and sent with the intent to the new activity.
+                Log.d(TAG, "onClick: " + "Button Working");
+                socket.emit("createSession");
+                socket.on("createdSession", new Emitter.Listener() {
+                    @Override
+                    public void call(Object... args) {
+                        Log.d(TAG, "call: HERE");
+                        JSONObject jsonObject = (JSONObject) args[0];
+                        try {
+                            Boolean success = jsonObject.getBoolean("success");
+                            if (success) {
+                                Intent i = new Intent(MainActivity.this, Drawing.class);
+                                i.putExtra("sessionId", jsonObject.getString("sessionId"));
+                                startActivity(i);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
             }
         });
         joinExistingSession.setOnClickListener(new View.OnClickListener() {
